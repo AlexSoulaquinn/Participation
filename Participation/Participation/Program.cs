7d4027f4d7bfc8390869c321ab6c9165bb0bb833
Alex Quinn <Alex.Quinn@ou.edu>
2020-01-22 11:37:08 -0600

    Implemented add methods. One for an array and another for static values.

--- Participation/Participation/Program.cs
+++ Participation/Participation/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// Alex Quinn
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,36 @@ namespace Participation
     {
         static void Main(string[] args)
         {
+            double num1, num2, num3;
+            num1 = 18;
+            num2 = 482.2;
+            num3 = 382.222;
+            double result = Add(5, 17.5);
+            Console.WriteLine($"5 + 17.5 = {result.ToString("N2")}");
+            result = Add(num1, num2);
+            Console.WriteLine($"{num1} + {num2} = {result}");
+
+            double[] nums = { num1, num2, num3 };
+
+
+            Console.ReadKey();
+
+        }
+        static double Add(double[] numToAdd)
+        {
+            double r = 0;
+
+            foreach (var number in numToAdd)
+            {
+                r += number;
+            }
+
+            return r;
+        }
+
+        static double Add(double x, double y)
+        {
+            return x + y;
         }
     }
 }
