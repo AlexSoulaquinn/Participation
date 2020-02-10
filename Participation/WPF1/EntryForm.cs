5f08410621ff619fe4cb71c8715d363013cfd4d1
Alex Quinn <Alex.Quinn@ou.edu>
2020-02-10 11:43:02 -0600

    Added new project

---
+++ Participation/WPF1/EntryForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1
+{
+    class EntryForm
+    {
+        string name { get; set; }
+        string address { get; set; }
+        int zipCode { get; set; }
+
+        public EntryForm()
+        {
+            name = "";
+            address = "";
+            zipCode = 0;
+        }
+
+        public EntryForm(string Name, string Address, int ZipCode)
+        {
+            name = Name;
+            address = Address;
+            zipCode = ZipCode;
+        }
+
+        public override string ToString()
+        {
+            string phrase = ($"{name} // {address} // {zipCode}");
+            return phrase;
+        }
+
+    }
+}
---
+++ Participation/WPF1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// Author: Alex Quinn
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+            EntryForm newEntry = new EntryForm(txtName.Text, txtAddress.Text, Convert.ToInt32(txtZip.Text));
+            lstApplications.Items.Add(newEntry);
+        }
+    }
+}
